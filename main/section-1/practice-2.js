4b53a055c42012defdecf44cee9ff082637c189b
xjhxcc <2351889342@qq.com>
2019-07-10 22:19:55 +0800

    refactor:not correct

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  // let collb=collectionB.map(item=item[0]);
+  // return collectionA.filter(item=>collectionB.includes(item));
+  return collectionA.filter(item=>collectionB.item);
 }
